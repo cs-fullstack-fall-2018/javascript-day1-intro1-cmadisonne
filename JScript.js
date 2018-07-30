42e3fae1c59d0547e0f9c18438996f37d4d2062a
Lotus <cmadisonne@gmail.com>
2018-07-30 11:26:19 -0500

    Day 1 JavaScript

---
+++ JScript.js
@@ -0,0 +1,10 @@
+// Exercise 1 //
+console.log("Hello World");
+
+// Exercise 2 //
+var userInput = prompt("Enter something");
+console.log(userInput);
+
+// Exercise 3 //
+var userInput2 = prompt("What is your name?");
+console.log("Hello "+ userInput2);
