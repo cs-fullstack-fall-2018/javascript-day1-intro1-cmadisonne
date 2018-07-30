41e6ba467589694bce969bb696bde762faf8f1af
Lotus <cmadisonne@gmail.com>
2018-07-30 11:38:17 -0500

    JavaScript Day1

---
+++ Ex2.js
@@ -0,0 +1,3 @@
+// Exercise 2 //
+var userInput = prompt("Enter something");
+console.log(userInput);
---
+++ Ex3.js
@@ -0,0 +1,3 @@
+// Exercise 3 //
+var userInput2 = prompt("What is your name?");
+console.log("Hello "+ userInput2);
